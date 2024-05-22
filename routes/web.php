a164532e95ee3159285fa8907f4c779446d0ea0c
noah-itComunica <noah-it@comunica.dk>
2024-05-22 09:56:38 +0200

    fixed problem with auth.php

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+require __DIR__.'/auth.php';
+
+
 Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/{user:username}', [ProfileController::class, 'index'])->name('profile');
-
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-require __DIR__.'/auth.php';
