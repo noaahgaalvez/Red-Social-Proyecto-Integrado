4efcde474f03fb0959a0b0f3985518aeec0ad865
Noah <ngalmon076@iesfuengirola.es>
2024-06-11 13:43:00 +0200

    Modo oscuro en responsive

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -142,11 +142,22 @@ function toggleDarkMode() {
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                         <template v-if="authUser">
-                            <div class="px-4">
-                                <div class="font-medium text-base text-gray-800 dark:text-gray-200">
-                                    {{ authUser.name }}
+                            <div class="flex items-center px-4 justify-between">
+                                <div>
+                                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">
+                                        {{ authUser.name }}
+                                    </div>
+                                    <div class="font-medium text-sm text-gray-500">{{ authUser.email }}</div>
                                 </div>
-                                <div class="font-medium text-sm text-gray-500">{{ authUser.email }}</div>
+
+                                <button @click="toggleDarkMode">
+                                    <svg v-if="dark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-yellow-500">
+                                        <path fill-rule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clip-rule="evenodd" />
+                                    </svg>
+                                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-yellow-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+                                    </svg>  
+                                </button>
                             </div>
     
                             <div class="mt-3 space-y-1">
